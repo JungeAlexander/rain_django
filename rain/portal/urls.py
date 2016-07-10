c3ec5313b49eb0ad4911f129947a087fb8fea0b6
Alexander Junge <ajunge@rth.dk>
2016-07-10 20:58:43 +0200

    Add url calls for dummy RNA and Interaction views

--- rain/portal/urls.py
+++ rain/portal/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # ex: /portal/interaction/5/
+    url(r'^interaction/(?P<interaction_id>[0-9]+)/$', views.interaction_detail, name='interaction_detail'),
+    # ex: /portal/rna/3/
+    url(r'^rna/(?P<rna_id>[0-9]+)/$', views.rna_detail, name='rna_detail'),
+    # ex: /portal/rna/3/description/
+    url(r'^rna/(?P<rna_id>[0-9]+)/description/$', views.rna_description, name='rna_description'),
 ]
