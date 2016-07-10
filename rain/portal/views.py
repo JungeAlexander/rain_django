3f52e2aaef2ac59c079706dce0cb6b42219c7ba9
Alexander Junge <ajunge@rth.dk>
2016-07-10 20:59:03 +0200

    Add dummy RNA and Interaction views

--- rain/portal/views.py
+++ rain/portal/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the portal index.")
+
+
+def interaction_detail(request, interaction_id):
+    return HttpResponse("You're looking at interaction %s." % interaction_id)
+
+
+def rna_detail(request, rna_id):
+    return HttpResponse("You're looking at RNA %s." % rna_id)
+
+
+def rna_description(request, rna_id):
+    return HttpResponse("You're looking at the description RNA %s." % rna_id)
