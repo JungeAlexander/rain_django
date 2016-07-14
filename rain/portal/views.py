f66e15f8f6b8b76e88af816e50c4a8daf4c9dcc6
Alexander Junge <ajunge@rth.dk>
2016-07-14 18:17:03 +0200

    New voting options for RNA aliases

--- rain/portal/views.py
+++ rain/portal/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, Http404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
-from .models import Interaction, RNA
+from .models import Interaction, RNA, RNAalias
 
 # def index(request):
 #     interactions = Interaction.objects.all()
@@ -27,3 +28,27 @@ def rna_detail(request, rna_id):
 
 def rna_description(request, rna_id):
     return HttpResponse("You're looking at the description RNA %s." % rna_id)
+
+
+def vote(request, rna_id):
+    rna = get_object_or_404(RNAalias, pk=rna_id)
+    try:
+        selected_choice = rna.rnaalias_set.get(pk=request.POST['choice'])
+    except (KeyError, RNAalias.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'portal/rna_detail.html', {
+            'rna': rna,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('portal:results', args=(rna.id,)))
+
+
+def results(request, rna_id):
+    rna = get_object_or_404(RNA, pk=rna_id)
+    return render(request, 'polls/results.html', {'rna': rna})
