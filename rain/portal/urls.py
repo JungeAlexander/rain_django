8c6e338eb23ad3eb88e7557cfc6a3276ee5a874a
Alexander Junge <ajunge@rth.dk>
2016-07-15 15:55:30 +0200

    Changes to URLconf before making use of generic views

--- rain/portal/urls.py
+++ rain/portal/urls.py
@@ -4,16 +4,15 @@ from . import views
 
 app_name = 'portal'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /portal/interaction/5/
     url(r'^interaction/(?P<interaction_id>[0-9]+)/$', views.interaction_detail, name='interaction_detail'),
     # ex: /portal/rna/3/
-    url(r'^rna/(?P<rna_id>[0-9]+)/$', views.rna_detail, name='rna_detail'),
+    url(r'^rna/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='rna_detail'),
     # ex: /portal/rna/3/description/
     url(r'^rna/(?P<rna_id>[0-9]+)/description/$', views.rna_description, name='rna_description'),
     # ex: /portal/rna/3/vote/
     url(r'^rna/(?P<rna_id>[0-9]+)/vote/$', views.vote, name='vote'),
-    # TODO remove? was not part of tutorial
     # ex: /portal/rna/3/results/
-    url(r'^rna/(?P<rna_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^rna/(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 ]
