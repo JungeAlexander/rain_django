a8b9457cc44afcd9bcd6fad983b54f1badb71b65
Alexander Junge <ajunge@rth.dk>
2016-07-15 17:00:05 +0200

    Add two simple test cases for is_mirna()

--- rain/portal/tests.py
+++ rain/portal/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import RNA
+
+
+class RNAMethodTests(TestCase):
+
+    def test_is_mirna_with_smirna(self):
+        """
+        is_mirna() should return False for identifier smiRNA
+        """
+        smirna = RNA(identifier='smiRNA')
+        self.assertEqual(smirna.is_mirna(), False)
+
+
+    def test_is_mirna_with_mirna(self):
+        """
+        is_mirna() should return False for identifier smiRNA
+        """
+        mirna = RNA(identifier='miRNA-')
+        self.assertEqual(mirna.is_mirna(), True)
