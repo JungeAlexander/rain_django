c0161d7484b826b13a4cea40ba51251bf786c724
Alexander Junge <ajunge@rth.dk>
2016-07-16 10:47:23 +0200

    Add basic tests for portal index view
    RNAs with empty description should not be displayed on index page
    while those with a non-empty description should be displayed.

--- rain/portal/tests.py
+++ rain/portal/tests.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from .models import RNA
@@ -12,10 +13,48 @@ class RNAMethodTests(TestCase):
         smirna = RNA(identifier='smiRNA')
         self.assertEqual(smirna.is_mirna(), False)
 
-
     def test_is_mirna_with_mirna(self):
         """
         is_mirna() should return False for identifier smiRNA
         """
         mirna = RNA(identifier='miRNA-')
         self.assertEqual(mirna.is_mirna(), True)
+
+
+def create_rna(identifier, description):
+    """
+    Creates a RNA with the given `identifier` and `description`.
+    """
+    return RNA.objects.create(identifier=identifier, description=description)
+
+
+class RNAViewTests(TestCase):
+    def test_index_view_with_no_rnas(self):
+        """
+        If no RNAs exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('portal:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No entities found.")
+        self.assertQuerysetEqual(response.context['entity_list'], [])
+
+    def test_index_view_with_an_rna_with_description(self):
+        """
+        RNA with a non-empty description should be displayed.
+        """
+        create_rna(identifier="myRNA", description='This is myRNA.')
+        response = self.client.get(reverse('portal:index'))
+        self.assertQuerysetEqual(
+            response.context['entity_list'],
+            ['<RNA: myRNA>']
+        )
+
+    def test_index_view_with_an_rna_without_description(self):
+        """
+        RNA with an empty description should not be displayed.
+        """
+        create_rna(identifier="myRNA", description='')
+        response = self.client.get(reverse('portal:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No entities found.")
+        self.assertQuerysetEqual(response.context['entity_list'], [])
