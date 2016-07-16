9176b8bd5752d68235f649e34b3571ca6b1933e8
Alexander Junge <ajunge@rth.dk>
2016-07-16 11:03:27 +0200

    Test detail view for RNAs in portal app
    RNAs without a description should not be accessible in detail view;
    RNAs with a description should.

--- rain/portal/tests.py
+++ rain/portal/tests.py
@@ -58,3 +58,26 @@ class RNAViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No entities found.")
         self.assertQuerysetEqual(response.context['entity_list'], [])
+
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_an_rna_without_description(self):
+        """
+        The detail view of an RNA without description should
+        return a 404 not found.
+        """
+        rna = create_rna(identifier='XYZ', description='')
+        url = reverse('portal:rna_detail', args=(rna.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_an_rna_with_description(self):
+        """
+        The detail view of an RNA without description should
+        display the RNA identifier and description.
+        """
+        rna = create_rna(identifier="myRNA", description='This is myRNA.')
+        url = reverse('portal:rna_detail', args=(rna.id,))
+        response = self.client.get(url)
+        self.assertContains(response, rna.identifier)
+        self.assertContains(response, rna.description)
