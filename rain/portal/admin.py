3167f1709ac5437af5378d8fef0eb2771fcbd80a
Alexander Junge <ajunge@rth.dk>
2016-07-18 21:48:18 +0200

    Split admin view for Interactions into two field sets

--- rain/portal/admin.py
+++ rain/portal/admin.py
@@ -3,6 +3,10 @@ from .models import Interaction
 
 
 class InteractionAdmin(admin.ModelAdmin):
-    fields = ['views', 'entity1', 'entity2']
+
+    fieldsets = [
+        (None,               {'fields': ['views']}),
+        ('Interacting entities', {'fields': ['entity1', 'entity2']}),
+    ]
 
 admin.site.register(Interaction, InteractionAdmin)
