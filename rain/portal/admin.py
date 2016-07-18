dc53b7c071b23550e48395be80786c1982500136
Alexander Junge <ajunge@rth.dk>
2016-07-18 21:45:26 +0200

    Change field order in admin view for Interaction

--- rain/portal/admin.py
+++ rain/portal/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Interaction
 
-admin.site.register(Interaction)
+
+class InteractionAdmin(admin.ModelAdmin):
+    fields = ['views', 'entity1', 'entity2']
+
+admin.site.register(Interaction, InteractionAdmin)
